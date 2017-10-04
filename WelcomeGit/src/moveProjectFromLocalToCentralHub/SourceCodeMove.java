c2305d0f7ad41c04dc49bd688fce34371df0cf15
udhayalibom <rayyavu@yahoo.com>
2017-10-04 16:55:04 -0400

    Welcome to Git Central Repository

---
+++ WelcomeGit/src/moveProjectFromLocalToCentralHub/SourceCodeMove.java
@@ -0,0 +1,12 @@
+package moveProjectFromLocalToCentralHub;
+
+public class SourceCodeMove {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to Practice session");
+
+	}
+
+}
